50a3680e7be2ff028be3bf05a8307f7f186aa8ed
Sheep Extraordinaire <76109159+rosehairedsheep@users.noreply.github.com>
2023-10-14 23:34:17 +0300

    instantiating tiles

--- Scenes/play_board.gd
+++ Scenes/play_board.gd
@@ -3,7 +3,17 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	var screenWidth = get_viewport().size.x
+	var screenHeight = get_viewport().size.y
+	var tile = load("res://Scenes/tile_square.tscn")
+	var bbee = load("res://Scenes/bumblesheep.tscn")
+	for i in range(8):
+		for j in range(8):
+			var tileInstance = tile.instantiate()
+			var bbeeInstance = bbee.instantiate()
+			tileInstance.position = Vector2((5*screenWidth/6)+100*i, (screenHeight/10)+100+100*j)
+			add_child(tileInstance)
+			tileInstance.add_child(bbeeInstance)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
--- Scenes/play_space.gd
+++ Scenes/play_space.gd
@@ -3,7 +3,7 @@ extends Node2D
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	pass # Replace with function body.
+	DisplayServer.window_set_mode(DisplayServer.WINDOW_MODE_FULLSCREEN)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
