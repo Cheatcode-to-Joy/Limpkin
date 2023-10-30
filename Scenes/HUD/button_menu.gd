56d306ad79aa40bd5baa21b07314c62b5cb23ba5
rosehairedsheep <pinkhairedsheep@gmail.com>
2023-10-30 10:43:19 +0200

    menu button

---
+++ Scenes/HUD/button_menu.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+func _ready():
+	scale = Vector2(45,20)
+
+func _process(_delta):
+	pass
--- Scenes/HUD/play_hud.gd
+++ Scenes/HUD/play_hud.gd
@@ -1,6 +1,7 @@
 extends Node2D
 
 var buttonExit
+var buttonMenu
 
 func _ready():
 	buttonExit = preload("res://Scenes/HUD/button_exit.tscn")
@@ -8,6 +9,12 @@ func _ready():
 	add_child(thisButtonExit)
 	thisButtonExit.scale = Vector2(0.2,0.2)
 	thisButtonExit.position = Vector2(get_viewport().size.x-500,get_viewport().size.y-300)
+	
+	buttonMenu = preload("res://Scenes/HUD/button_menu.tscn")
+	var thisButtonMenu = buttonMenu.instantiate()
+	add_child(thisButtonMenu)
+	thisButtonMenu.scale = Vector2(0.2,0.2)
+	thisButtonMenu.position = Vector2(get_viewport().size.x-500,get_viewport().size.y-400)
 
 func _process(_delta):
 	pass
